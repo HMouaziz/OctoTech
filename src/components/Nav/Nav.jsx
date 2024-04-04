26f38b86758980dd5e36ae7bfe0efec87612e235
hmoua <hmouaziz@project-hephaestus.com>
2024-04-05 00:32:37 +0100

    Add navigation to application
    The code changes implement a new navigation feature for the application. Using "react-router-dom", a navigation bar was created at the bottom of the application, allowing users to navigate between the HomePage and EnergyPage. The existing code in App.jsx was replaced with routing parameters to direct users to the appropriate pages based on the selected navigation link.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 export const Nav = () => {
   return (
-    <></>
-  )
-}
+    <nav className="navbar is-fixed-bottom">
+      <NavLink to="/" className="navbar-link">
+        Home
+      </NavLink>
+      <NavLink to="/energy" className="navbar-link">
+        Energy
+      </NavLink>
+    </nav>
+  );
+};
