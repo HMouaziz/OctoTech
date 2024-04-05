bc8ef3c265a3c7af1267b99cc9b7b99c288dfe39
mariam1798 <Mariam.alneamah@gmail.com>
2024-04-05 11:46:24 +0100

    add styling for info page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Nav } from "./components/Nav/Nav.jsx";
 import { Homepage } from "./Pages/Homepage/Homepage.jsx";
 import { EnergyPage } from "./Pages/EnergyPage/EnergyPage.jsx";
+import InfoPage from "./Pages/InfoPage/InfoPage.jsx";
+import "./App.scss";
 
 function App() {
   return (
@@ -9,8 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/energy" element={<EnergyPage />} />
+        <Route path="/info" element={<InfoPage />} />
       </Routes>
-      <Nav/>
+      <Nav />
     </BrowserRouter>
   );
 }
