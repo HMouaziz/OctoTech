68febe2fd6775bcb2571df48b16aaf122e81965a
tnp_167 <tnp_167@outlook.com>
2024-04-05 14:06:05 +0100

    Format and style navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,37 @@
-import { NavLink } from "react-router-dom";
-
+import { NavLink , useLocation } from "react-router-dom";
+import "./Nav.scss";
 export const Nav = () => {
+
+  const location = useLocation();
+  const currentRoute = location.pathname;
+
+  const getNavbarColor = (currentRoute, index) => {
+    const routes = [
+      { path: '/', index: 0, select: '--selected' },
+      { path: '/info', index: 0, select: '--selected' },
+      { path: '/energy', index: 1, select: '--selected' },
+      { path: '/payments', index: 2, select: '--selected' }
+    ];
+  
+    const matchedRoute = routes.find(route => {
+      return route.path === currentRoute && route.index === index;
+    });
+  
+    return matchedRoute ? matchedRoute.select : '';
+  };
   return (
-    <nav className="navbar is-fixed-bottom">
-      <NavLink to="/" className="navbar-link">
+    <nav className="navbar">
+      <NavLink to="/" className={`navbar__link navbar__link--home navbar__link${getNavbarColor(currentRoute, 0)}`}>
+       <div className={`navbar__home-icon navbar__home-icon${getNavbarColor(currentRoute, 0)}`}></div>
         Home
       </NavLink>
-      <NavLink to="/energy" className="navbar-link">
+      <NavLink to="/energy" className={`navbar__link navbar__link--energy navbar__link${getNavbarColor(currentRoute, 1)}`}>
+        <div className={`navbar__energy-icon navbar__energy-icon${getNavbarColor(currentRoute, 1)}`}></div>
         Energy
+      </NavLink>
+      <NavLink to="/payments" className="navbar__link navbar__link--payment">
+      <div className={`navbar__payments-icon navbar__energy-icon${getNavbarColor(currentRoute, 2)}`}></div>
+        Payments
       </NavLink>
     </nav>
   );
